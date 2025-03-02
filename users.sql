20b4eba6749e4268b0d303dd011f811e699682fb
justin11224 <129360007+justin11224@users.noreply.github.com>
2025-03-02 21:58:07 +0800

    Add files via upload

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2025 at 02:13 PM
+-- Generation Time: Feb 28, 2025 at 02:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,17 +36,19 @@ CREATE TABLE `users` (
   `Password` varchar(200) NOT NULL,
   `Role` varchar(50) NOT NULL,
   `Contact` varchar(99) NOT NULL,
-  `Status` varchar(20) NOT NULL
+  `Status` varchar(20) NOT NULL,
+  `Username` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `Fname`, `MidName`, `Lname`, `Email`, `Password`, `Role`, `Contact`, `Status`) VALUES
-(1, 'Nush', 'S.', 'Sanchez', 'Nush1@gmai.com', 'Nush123', 'Saller', '09519839203', 'Pending'),
-(3, 'Francis', '', 'Avila', 'avila79@gmail.com', 'avila0985', 'Buyer', '09124758943', 'Pending'),
-(4, 'cidric', '', 'sanchez', '123@gmail.com', '123123123', 'Admin', '123123123123', 'Pending');
+INSERT INTO `users` (`user_id`, `Fname`, `MidName`, `Lname`, `Email`, `Password`, `Role`, `Contact`, `Status`, `Username`) VALUES
+(1, 'Nush', 'S.', 'Sanchez', 'Nush1@gmai.com', 'Nush123', 'Saller', '09519839203', 'Pending', 'nush90'),
+(3, 'Francis', '', 'Avila', 'avila79@gmail.com', 'avila0985', 'Buyer', '09124758943', 'Active', 'cis12'),
+(4, 'josh', '', 'sanchez', '123@gmail.com', '123123123', 'Admin', '123123123123', 'Active', 'cid123'),
+(5, 'cidric', 'S', 'sanchez', 'just47969@gmail.com', 'sanchez123', 'Buyer', '0961912312', 'Pending', 'justin');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
